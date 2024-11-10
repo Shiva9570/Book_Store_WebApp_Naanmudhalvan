e6590b9dec9fab35158f6acbcd0484d6df902649
shibendra narayan mishra <116627313+Shiva9570@users.noreply.github.com>
2024-11-10 22:10:07 +0530

    commit

---
+++ server/db/config.js
@@ -0,0 +1,8 @@
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Shibendra007:Shibendra852@naanproject.aopnk.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
